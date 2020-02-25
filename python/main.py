2650dcd01ab6df93a705facb8936a2048f6df212
Geoffrey Jount <geoffrey.jount@epita.fr>
2020-02-25 19:43:15 +0100

    Add combine() function.

--- python/main.py
+++ python/main.py
@@ -1,5 +1,19 @@
+import itertools
 from Image import Image
 from GreyImage import GreyImage
+
+
+def combine(r, g, b):
+    im = Image()
+    im.height = r.height
+    im.width = r.width
+    for (r_line, g_line, b_line) in zip(r.matrix, g.matrix, b.matrix):
+        line = []
+        for (r_p, g_p, b_p) in zip(r_line, g_line, b_line):
+            line.append((r_p, g_p, b_p))
+        im.matrix.append(line)
+    return im
+
 
 
 def main():
@@ -7,27 +21,36 @@ def main():
     filename = 'media/20140712_163729.tga'
     im = Image(filename)
 
-    print("Convert to grayscale")
-    gray = GreyImage(im)
+    print("Get components")
+    r, g, b = im.ComponentsRGB()
 
-    print("Save gray image")
-    # gray.save("gray.png")
-    gray.show()
+    print("Combine components")
+    new_im = combine(r, g, b)
 
-    print("Save histogram")
-    # gray.saveHistogram("histo_gray.png")
-    gray.showHistogram()
+    im.show()
+    new_im.show()
 
-    print("Equalize")
-    gray.equalizeHistogram()
+    # print("Convert to grayscale")
+    # gray = GreyImage(im)
 
-    print("Save equalized image")
-    # gray.save("equal_gray.png")
-    gray.show()
+    # print("Save gray image")
+    # # gray.save("gray.png")
+    # gray.show()
 
-    print("Save equalized histogram")
-    # gray.saveHistogram("histo_equal_gray.png")
-    gray.showHistogram()
+    # print("Save histogram")
+    # # gray.saveHistogram("histo_gray.png")
+    # gray.showHistogram()
+
+    # print("Equalize")
+    # gray.equalizeHistogram()
+
+    # print("Save equalized image")
+    # # gray.save("equal_gray.png")
+    # gray.show()
+
+    # print("Save equalized histogram")
+    # # gray.saveHistogram("histo_equal_gray.png")
+    # gray.showHistogram()
 
 
 
